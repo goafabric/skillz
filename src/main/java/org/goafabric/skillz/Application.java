0979da5957f38ed988da35ca8ec11b9eb9771c9e
goafabric <amautsch@gmx.de>
2019-05-01 20:05:01 +0200

    refactoring

--- src/main/java/org/goafabric/skillz/Application.java
+++ src/main/java/org/goafabric/skillz/Application.java
@@ -1,12 +1,15 @@
 package org.goafabric.skillz;
 
 import org.goafabric.skillz.persistence.AddressBo;
+import org.goafabric.skillz.persistence.AddressRepository;
 import org.goafabric.skillz.persistence.PersonBo;
 import org.goafabric.skillz.persistence.PersonRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import javax.transaction.Transactional;
 
 /**
  * Created by amautsch on 26.06.2015.
@@ -20,9 +23,21 @@ public class Application {
     }
 
     @Bean
-    public CommandLineRunner loadData(PersonRepository repository) {
+    @Transactional
+    public CommandLineRunner loadData(
+            PersonRepository repository, AddressRepository addressRepository) {
         return (args) -> {
-            // save a couple of customers
+
+            /*
+            repository.save(PersonBo.builder()
+                    .firstName("Homer").lastName("Simpson")
+                    .address(addressRepository.save(
+                            AddressBo.builder()
+                                    .street("Evergreen Terace 1").city("Springfield")
+                                    .build()))
+                    .build());
+             */
+
             repository.save(PersonBo.builder()
                     .firstName("Homer").lastName("Simpson")
                         .address(AddressBo.builder()
@@ -42,6 +57,8 @@ public class Application {
                             .street("Monty Mansion").city("Springfield")
                             .build())
                     .build());
+
+
         };
     }
 }
